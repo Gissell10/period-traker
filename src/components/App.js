fa9a74c2b29cb7224c5c8bcdd5a970338f889a52
isabelsousac <isabel.sousa15246@gmail.com>
2022-11-19 22:58:13 +1100

    User sign up form was created with function for password and email validation. App component can send html post request for creating a new user

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,6 @@
+import { Component } from "react";
+import axios from 'axios'
+
 import Nav from "./Nav";
 import Home from "./Home";
 import Users from "./Users";
@@ -5,17 +8,45 @@ import AboutYou from "./AboutYou";
 import Schedule from "./Schedule";
 import Symptoms from "./Symptoms";
 
-function App() {
-  return (
-    <div>
-      <Nav />
-      <Home />
-      <Users />
-      <AboutYou />
-      <Schedule />
-      <Symptoms />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    user: {}
+  }
+
+  signUp = (user) => {
+    const body = JSON.stringify({
+      user: {
+        first_name: user.firstName,
+        last_name: user.lastName,
+        email: user.email,
+        password: user.password,
+      }
+    });
+    axios.post('http://localhost:3000/users', body, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      return this.setState({user: response.data})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.user.firstName ? <h4>Welcome {this.state.user.firstName}</h4> : <Users signUp={this.signUp}/>}
+        <Nav />
+        <Home />
+        <Users />
+        <AboutYou />
+        <Schedule />
+        <Symptoms />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,118 @@
 import React, { useState } from "react";
 
-const Users = function () {
+const Users = function (props) {
+  const [signUpInput, setSignUpInput] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+  const [signUpInputError, setSignUpInputError] = useState({
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+  const handleSignUp = () => {
+    props.signUp(signUpInput)
+  }
+
+  const handleChange = (name, value) => {
+    setSignUpInput({
+      ...signUpInput, [name]: value // for all sign up input states, set the name (key) to value
+    });
+  }
+
+  const validateSignUp = (e) => {
+    e.preventDefault();
+
+    let inputError = { // same as signup inputError
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+
+    if (!signUpInput.email) {
+      setSignUpInputError({
+        ...inputError, email: 'Enter a valid email'
+      });
+      return;
+    }
+
+    if (signUpInput.password !== signUpInput.passwordConfirmation) {
+      setSignUpInputError({
+        ...inputError, passwordConfirmation: 'Both passwords should match'
+      });
+      return;
+    }
+
+    if (!signUpInput.password) {
+      setSignUpInputError({
+        ...inputError, password: 'Password should not be empty'
+      });
+      return;
+    }
+
+    if (Object.values(signUpInputError).every(input => input === '')) {
+      handleSignUp()
+    }
+
+    setSignUpInputError(inputError)
+  }
+
+  
   return (
     <div>
-      <form action="">
-        <input type="text" />
-        <label></label>
-        <button> sing in</button>
+      <h1>Sign Up</h1>
+      <form onSubmit={validateSignUp}>
+        <label>First Name</label>
+        <input
+          name="firstName"
+          value={signUpInput.firstName} 
+          onChange={({target}) => {handleChange(target.name, target.value)}}
+        />
+        
+        <label>Last Name</label>
+        <input
+          name="lastName" 
+          value={signUpInput.lastName} 
+          onChange={({target}) => handleChange(target.name, target.value)}
+        />
+
+        <label>Email</label>
+        <input
+          name="email" 
+          type="email" 
+          value={signUpInput.email} 
+          onChange={({target}) => handleChange(target.name, target.value)}
+        />
+        <p className="error-message">{signUpInputError.email}</p>
+
+         
+        <label>Password</label>
+        <input
+          name="password" 
+          type="password" 
+          value={signUpInput.password} 
+          onChange={({target}) => handleChange(target.name, target.value)}
+        />
+        <p className="error-message">{signUpInputError.password}</p>
+
+        <label>Password Confirmation</label>
+        <input
+          name="passwordConfirmation" 
+          type="password" 
+          value={signUpInput.passwordConfirmation} 
+          onChange={({target}) => handleChange(target.name, target.value)}
+        />
+        <p className="error-message">{signUpInputError.passwordConfirmation}</p>
+
+        <button>Sign Up</button>
       </form>
     </div>
   );
 };
+
 export default Users;
