14dc4ff93850d5068dc2a5a47960bde0237374b5
isabelsousac <isabel.sousa15246@gmail.com>
2022-11-24 15:36:55 +1100

    Gets cycles index of individual user

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,53 +2,22 @@ import { useEffect, useState } from "react";
 
 export const useCycleData = ({ userId }) => {
   const [cycleData, setCycleData] = useState([]);
-
-  console.log("cycle data for user ", userId);
-
+  const token = localStorage.getItem("token");
   useEffect(() => {
-    let token = localStorage.getItem("token");
-    console.log(token);
-    if (token) {
-      // const requestOptions = {
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     Authorization: `Bearer ${token}`
-      //   }
-      // };
-      fetch("http://localhost:3000/cycles", {
-        method: "GET",
+    fetch("http://localhost:3000/cycles", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setCycleData(json)
       })
-        .then((response) => response.json())
-        .then((json) => {
-          setCycleData(json.cycles);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    setCycleData([
-      {
-        id: 1,
-        period_start: new Date(2021, 6, 1),
-        period_end: new Date(2021, 6, 16),
-      },
-      {
-        id: 2,
-        period_start: new Date(2021, 5, 1),
-        period_end: new Date(2021, 5, 3),
-      },
-      {
-        id: 13,
-        period_start: new Date(2021, 3, 1),
-        period_end: new Date(2021, 3, 12),
-      },
-      {
-        id: 4,
-        period_start: new Date(2021, 2, 1),
-        period_end: new Date(2021, 2, 7),
-      },
-    ]);
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return cycleData;
